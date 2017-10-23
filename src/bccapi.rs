1fa6737939a503d54530812616e161620d945bc5
Zhiting Zhu <zhitingz@cs.utexas.edu>
2017-10-23 12:08:40 -0500

    use regex

--- src/bccapi.rs
+++ src/bccapi.rs
@@ -300,6 +300,34 @@ pub type __u16 = ::std::os::raw::c_ushort;
 pub type __s32 = ::std::os::raw::c_int;
 pub type __u32 = ::std::os::raw::c_uint;
 pub type __u64 = ::std::os::raw::c_ulonglong;
+pub const BPF_REG_0: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_0;
+pub const BPF_REG_1: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_1;
+pub const BPF_REG_2: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_2;
+pub const BPF_REG_3: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_3;
+pub const BPF_REG_4: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_4;
+pub const BPF_REG_5: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_5;
+pub const BPF_REG_6: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_6;
+pub const BPF_REG_7: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_7;
+pub const BPF_REG_8: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_8;
+pub const BPF_REG_9: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_9;
+pub const BPF_REG_10: _bindgen_ty_1 = _bindgen_ty_1::BPF_REG_10;
+pub const __MAX_BPF_REG: _bindgen_ty_1 = _bindgen_ty_1::__MAX_BPF_REG;
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum _bindgen_ty_1 {
+    BPF_REG_0 = 0,
+    BPF_REG_1 = 1,
+    BPF_REG_2 = 2,
+    BPF_REG_3 = 3,
+    BPF_REG_4 = 4,
+    BPF_REG_5 = 5,
+    BPF_REG_6 = 6,
+    BPF_REG_7 = 7,
+    BPF_REG_8 = 8,
+    BPF_REG_9 = 9,
+    BPF_REG_10 = 10,
+    __MAX_BPF_REG = 11,
+}
 #[repr(C)]
 #[derive(Debug, Default, Copy)]
 pub struct bpf_insn {
@@ -1762,6 +1790,30 @@ impl ::std::fmt::Debug for bpf_sock_ops {
                local_ip4 , self . remote_ip6 , self . local_ip6 , self .
                remote_port , self . local_port)
     }
+}
+pub const BPF_SOCK_OPS_VOID: _bindgen_ty_2 = _bindgen_ty_2::BPF_SOCK_OPS_VOID;
+pub const BPF_SOCK_OPS_TIMEOUT_INIT: _bindgen_ty_2 =
+    _bindgen_ty_2::BPF_SOCK_OPS_TIMEOUT_INIT;
+pub const BPF_SOCK_OPS_RWND_INIT: _bindgen_ty_2 =
+    _bindgen_ty_2::BPF_SOCK_OPS_RWND_INIT;
+pub const BPF_SOCK_OPS_TCP_CONNECT_CB: _bindgen_ty_2 =
+    _bindgen_ty_2::BPF_SOCK_OPS_TCP_CONNECT_CB;
+pub const BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB: _bindgen_ty_2 =
+    _bindgen_ty_2::BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB;
+pub const BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB: _bindgen_ty_2 =
+    _bindgen_ty_2::BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB;
+pub const BPF_SOCK_OPS_NEEDS_ECN: _bindgen_ty_2 =
+    _bindgen_ty_2::BPF_SOCK_OPS_NEEDS_ECN;
+#[repr(u32)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub enum _bindgen_ty_2 {
+    BPF_SOCK_OPS_VOID = 0,
+    BPF_SOCK_OPS_TIMEOUT_INIT = 1,
+    BPF_SOCK_OPS_RWND_INIT = 2,
+    BPF_SOCK_OPS_TCP_CONNECT_CB = 3,
+    BPF_SOCK_OPS_ACTIVE_ESTABLISHED_CB = 4,
+    BPF_SOCK_OPS_PASSIVE_ESTABLISHED_CB = 5,
+    BPF_SOCK_OPS_NEEDS_ECN = 6,
 }
 #[repr(C)]
 #[derive(Debug, Default, Copy)]
