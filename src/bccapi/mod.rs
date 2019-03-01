081e79b953b525d5636e0963d35132d6a4dbc694
Brian Martin <bmartin@twitter.com>
2019-03-01 11:15:57 -0800

    features misnamed in source
    * feature names in the source don't match resulting in defaulting to v0.8.0
    * hide warnings by conditionally including the per-version modules
    * extend the test matrix to test with other bcc versions

--- src/bccapi/mod.rs
+++ src/bccapi/mod.rs
@@ -1,34 +1,55 @@
+#[cfg(feature = "v0_4_0")]
 mod v0_4_0;
+
+#[cfg(feature = "v0_5_0")]
 mod v0_5_0;
+
+#[cfg(feature = "v0_6_0")]
 mod v0_6_0;
+
+#[cfg(feature = "v0_6_1")]
 mod v0_6_1;
+
+#[cfg(feature = "v0_7_0")]
 mod v0_7_0;
+
+#[cfg(feature = "v0_8_0")]
 mod v0_8_0;
 
-#[cfg(feature = "0.4.0")]
+#[cfg(not(any(
+    feature = "v0_4_0",
+    feature = "v0_5_0",
+    feature = "v0_6_0",
+    feature = "v0_6_1",
+    feature = "v0_7_0",
+    feature = "v0_8_0"
+)))]
+mod v0_8_0;
+
+#[cfg(feature = "v0_4_0")]
 pub use v0_4_0::*;
 
-#[cfg(feature = "0.5.0")]
+#[cfg(feature = "v0_5_0")]
 pub use v0_5_0::*;
 
-#[cfg(feature = "0.6.0")]
+#[cfg(feature = "v0_6_0")]
 pub use v0_6_0::*;
 
-#[cfg(feature = "0.6.1")]
+#[cfg(feature = "v0_6_1")]
 pub use v0_6_1::*;
 
-#[cfg(feature = "0.7.0")]
+#[cfg(feature = "v0_7_0")]
 pub use v0_7_0::*;
 
-#[cfg(feature = "0.8.0")]
+#[cfg(feature = "v0_8_0")]
 pub use v0_8_0::*;
 
 #[cfg(not(any(
-    feature = "0.4.0",
-    feature = "0.5.0",
-    feature = "0.6.0",
-    feature = "0.6.1",
-    feature = "0.7.0",
-    feature = "0.8.0"
+    feature = "v0_4_0",
+    feature = "v0_5_0",
+    feature = "v0_6_0",
+    feature = "v0_6_1",
+    feature = "v0_7_0",
+    feature = "v0_8_0"
 )))]
 pub use v0_8_0::*;
