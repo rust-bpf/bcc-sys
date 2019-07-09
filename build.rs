599cdb611a5795171156fd9a89bc2f6c821823b1
Brian Martin <bmartin@twitter.com>
2019-07-09 16:40:02 -0700

    fix static linking
    Previous PR didn't address the default version properly, resulting
    in linking still not working when no version specified via feature
    flag. This fix changes the logic in selecting which library to
    link so that we have a default case which is appropriate for the
    default supported bcc version

--- build.rs
+++ build.rs
@@ -135,11 +135,17 @@ fn linking_info() {
     println!("cargo:rustc-link-lib=static=bcc");
     println!("cargo:rustc-link-lib=static=bcc-loader-static");
     if cfg!(any(
-        feature = "v0_10_0",
+        feature = "v0_4_0",
+        feature = "v0_5_0",
+        feature = "v0_6_0",
+        feature = "v0_6_1",
+        feature = "v0_7_0",
+        feature = "v0_8_0",
+        feature = "v0_9_0",
     )) {
-        println!("cargo:rustc-link-lib=static=bcc_bpf");
-    } else {
         println!("cargo:rustc-link-lib=static=bpf");
+    } else {
+        println!("cargo:rustc-link-lib=static=bcc_bpf");
     }
     println!("cargo:rustc-link-lib=static=b_frontend");
     println!("cargo:rustc-link-lib=static=clang_frontend");
